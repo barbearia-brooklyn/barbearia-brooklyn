1228acc7c7225aa425c7b7abee13e5008f45af2d
Tiago Oliveira <geral@tiagoanoliveira.pt>
2025-12-19 19:51:51 +0000

    Simplify Google account unlink button rendering

--- public/js/profile.js
+++ public/js/profile.js
@@ -512,7 +512,7 @@ async function loadLinkedAccounts() {
                 <span><i class="fab fa-google"></i> Google</span>
                 ${accounts.includes('google')
             ? `<button class="btn-small btn-unlink" data-provider="google" data-has-password="${hasPassword}">Desassociar</button>`
-            : `<div class="oauth-button-wrapper"><button class="btn-small btn-link" data-provider="google">Associar</button><div class="oauth-single-overlay"><span>Em breve</span></div></div>`
+            : `<button class="btn-small btn-link" data-provider="google">Associar</button>`
         }
             </div>
             <div class="account-item">
